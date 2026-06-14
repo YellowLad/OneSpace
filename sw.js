7e9cecd4f47547af03adf133a305940409ccbe18
C A Coyne <chriscoyne143@gmail.com>
2026-06-15 00:45:05 +0100

    Create sw.js - Service Worker for offline support

---
+++ sw.js
@@ -0,0 +1,22 @@
+// Cache OneWorld for offline use
+const CACHE_NAME = 'oneworld-v1';
+const ASSETS = [
+  '/',
+  '/index.html'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .catch(() => console.log('Cache setup skipped'))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request)
+      .then(response => response || fetch(e.request))
+      .catch(() => caches.match('/index.html'))
+  );
+});
